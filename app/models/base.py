2d4e19627b4cde50e93d59f63ec525feddac4405
kumogire <kumogire@gmail.com>
2025-07-25 14:50:34 -0700

    Refactor base model

--- app/models/base.py
+++ app/models/base.py
@@ -1,3 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, DateTime, func
 
 Base = declarative_base()
+
+class TimestampMixin:
+    """Mixin to add created_at and updated_at timestamps"""
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
